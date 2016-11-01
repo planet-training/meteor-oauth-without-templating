e7f74b6c25eda939d159a4b3ddbe7cba9b3aefce
Max Bruchmann <bruchmann@signal7.de>
2016-11-02 00:19:17 +0100

    updated package deps

--- facebook-no-ui-conf/package.js
+++ facebook-no-ui-conf/package.js
@@ -1,20 +1,20 @@
 Package.describe({
-  summary: "Facebook OAuth flow without conf ui",
-  version: "0.0.8",
-  name : "planettraining:facebook-no-ui-conf"
+    summary: "Facebook OAuth flow without conf ui",
+    version: "0.0.9",
+    name: "planettraining:facebook-no-ui-conf"
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2@1.1.10', ['client', 'server']);
-  api.use('oauth@1.1.10', ['client', 'server']);
-  api.use('http@1.1.8', ['server']);
-  api.use('underscore@1.0.9', 'server');
-  api.use('random@1.0.10', 'client');
-  api.use('service-configuration@1.0.10', ['client', 'server']);
+    api.use('oauth2@1.1.11', ['client', 'server']);
+    api.use('oauth@1.1.11', ['client', 'server']);
+    api.use('http@1.2.10', ['server']);
+    api.use('underscore@1.0.10', 'server');
+    api.use('random@1.0.10', 'client');
+    api.use('service-configuration@1.0.11', ['client', 'server']);
 
-  api.export('Facebook');
+    api.export('Facebook');
 
 
-  api.addFiles('../meteor/packages/facebook/facebook_server.js', 'server');
-  api.addFiles('../meteor/packages/facebook/facebook_client.js', 'client');
+    api.addFiles('../meteor/packages/facebook/facebook_server.js', 'server');
+    api.addFiles('../meteor/packages/facebook/facebook_client.js', 'client');
 });
--- google-no-ui-conf/package.js
+++ google-no-ui-conf/package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  summary: "Google OAuth flow without conf ui",
-  version: "0.0.8",
-  name : "planettraining:google-no-ui-conf"
+    summary: "Google OAuth flow without conf ui",
+    version: "0.0.9",
+    name: "planettraining:google-no-ui-conf"
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2@1.1.10', ['client', 'server']);
-  api.use('oauth@1.1.10', ['client', 'server']);
-  api.use('http@1.1.8', ['server']);
-  api.use(['underscore@1.0.9', 'service-configuration@1.0.10'], ['client', 'server']);
-  api.use(['random@1.0.10'], 'client');
+    api.use('oauth2@1.1.11', ['client', 'server']);
+    api.use('oauth@1.1.11', ['client', 'server']);
+    api.use('http@1.2.10', ['server']);
+    api.use(['underscore@1.0.10', 'service-configuration@1.0.11'], ['client', 'server']);
+    api.use(['random@1.0.10'], 'client');
 
-  api.export('Google');
-  api.addFiles('../meteor/packages/google/google_server.js', 'server');
-  api.addFiles('../meteor/packages/google/google_client.js', 'client');
+    api.export('Google');
+    api.addFiles('../meteor/packages/google/google_server.js', 'server');
+    api.addFiles('../meteor/packages/google/google_client.js', 'client');
 });
